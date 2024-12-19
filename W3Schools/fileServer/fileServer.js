f41c5b1c13295d0382231ae8a4a02953aa4de493
porrako <kucmierz.bartosz09@gmail.com>
2024-12-20 00:27:15 +0100

    .

--- W3Schools/fileServer/fileServer.js
+++ W3Schools/fileServer/fileServer.js
@@ -3,16 +3,21 @@ var url = require("url");
 var fs = require("fs");
 
 http.createServer(function(req,res){
+    //This makes an object with a lot of usefull keys.
     var query = url.parse(req.url, true);
+
+    //This places a dot in front the url key pathname, pathname gives you /filename.
     var filename = "." + query.pathname;
 
+    //Reads the content of the file inserted into the URL.
     fs.readFile(filename,function(err,data){
         if(err){
             res.writeHead(404,{"Content-Type":"text/html"});
             return res.end("404 Not Found");
         }
         res.writeHead(200,{"Content-Type":"text/html"});
+        console.log(query);
         res.write(data);
-        res.end();
+        return res.end();
     });
 }).listen(8080);
